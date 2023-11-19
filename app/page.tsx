e42d81bf061c2b03d6ab388b2055761e460e4bf1
amar-eng <am24mo@gmail.com>
2023-11-18 20:29:17 -0500

    Some additional changes adding a bolting effect

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import {
   CodeIcon,
   ComponentBooleanIcon,
   Cross1Icon,
+  CrumpledPaperIcon,
   HeartFilledIcon,
+  LightningBoltIcon,
   SpeakerLoudIcon,
 } from '@radix-ui/react-icons';
 import Image from 'next/image';
@@ -23,13 +25,13 @@ export default function Home() {
     {
       header: 'Branding',
       bulletPoints: ['Market Research', 'Brand Strategy', 'Visual Identity'],
-      icon: <HeartFilledIcon color="red" />,
+      icon: <HeartFilledIcon color="#dc2626" height={25} width={25} />,
     },
     {
       header: 'Development',
 
       bulletPoints: ['Web Design', 'App Development', 'E-commerce Solutions'],
-      icon: <CodeIcon color="red" />,
+      icon: <CodeIcon color="#dc2626" height={25} width={25} />,
     },
 
     {
@@ -39,7 +41,7 @@ export default function Home() {
         'Performance Monitoring',
         'SEO Analysis',
       ],
-      icon: <BarChartIcon color="red" />,
+      icon: <BarChartIcon color="#dc2626" height={25} width={25} />,
     },
     {
       header: 'Marketing',
@@ -48,7 +50,7 @@ export default function Home() {
         'Social Media Management',
         'Content Marketing',
       ],
-      icon: <SpeakerLoudIcon color="red" />,
+      icon: <SpeakerLoudIcon color="#dc2626" height={25} width={25} />,
     },
   ];
 
@@ -66,9 +68,9 @@ export default function Home() {
 
         <div
           onClick={handleConsultationClick}
-          className=" text-center mt-4 right-0 top-0 flex w-full  cursor-pointer justify-center border-b border-red-500 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-red-500 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-extralight"
+          className=" text-center mt-4 right-0 top-0 flex w-full  cursor-pointer justify-center border-b border-red-500 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-red-500 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-light"
         >
-          Book a consultation call
+          Talk to us in the meantime
         </div>
       </div>
       {showCalendly && (
@@ -84,9 +86,20 @@ export default function Home() {
       )}
 
       <div className="relative mt-16 flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-red-200 after:via-red-600 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700 before:dark:opacity-10 after:dark:from-red-900 after:dark:via-[#ff0501] after:dark:opacity-40 before:lg:h-[360px] z-[-1] sm:mt-12 md:mt-1">
-        <h1 className="text-center text-red-500 text-4xl lg:text-6xl uppercase mb-16 ">
-          Coming Soon
-        </h1>
+        <div className="text-center text-4xl lg:text-6xl uppercase mb-16 flex items-center animate-moveRedColor">
+          <LightningBoltIcon
+            className="animate-lightning"
+            height={50}
+            width={50}
+          />
+          <div className="mx-4">Under Repair</div>
+
+          <LightningBoltIcon
+            height={50}
+            width={50}
+            className="animate-lightning"
+          />
+        </div>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -95,7 +108,7 @@ export default function Home() {
             key={index}
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           >
-            <h2 className={`mb-3 text-3xl font-semibold flex items-center`}>
+            <h2 className={`mb-3 text-2xl font-semibold flex items-center`}>
               <span className="mr-2 ">{data.icon}</span>
               {data.header}
             </h2>
